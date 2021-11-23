95cb6e5c98243e34247158137a4eb400af258fd9
Philippe Bernard <philippe.bernard@gmail.com>
2021-11-23 16:22:27 +0100

    A few experiments with the Twitter API

--- doit.ts
+++ doit.ts
@@ -1,3 +1,51 @@
 require('dotenv').config()
 
-console.log("TODO!");
+import { FollowersList, FollowersListParams, TwitterClient } from 'twitter-api-client';
+
+const doIt = async () => {
+  const twitterApiKey = process.env.TWITTER_API_KEY;
+  const twitterApiSecret = process.env.TWITTER_API_SECRET;
+  if (!twitterApiKey || !twitterApiSecret) {
+    console.log("ERROR: Twitter credentials are not configured");
+    return;
+  }
+
+  const twitterClient = new TwitterClient({
+    apiKey: twitterApiKey,
+    apiSecret: twitterApiSecret,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  });
+
+  /*
+  const user  = await twitterClient.accountsAndUsers.usersShow({ screen_name: '@ph_bernard' });
+  console.log(user);
+  */
+
+  /*
+  let nextCursor = undefined;
+  do {
+    console.log("Using " + nextCursor);
+    const params: FollowersListParams  = { screen_name: '@ph_bernard' };
+    if (nextCursor) {
+      params.cursor = nextCursor;
+    }
+    //const followers: FollowersList = await twitterClient.accountsAndUsers.followersList(params);
+    const followers: FollowersList = await twitterClient.accountsAndUsers.friendsList(params);
+    console.log(followers.users.map(u => u.name));
+    nextCursor = followers.next_cursor;
+  } while (nextCursor);
+  */
+
+  const data = await twitterClient.tweets.statusesUserTimeline({ screen_name: '@ph_bernard' });
+  console.log(data[0]);
+}
+
+(async () => {
+  try {
+    await doIt();
+  }
+  catch(e) {
+    console.log(e);
+  }
+})();
